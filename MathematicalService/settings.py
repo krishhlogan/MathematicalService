a4a11364ddebac39bcc109c799ec9083642a036d
krishnakumar <krishnakumar31096@gmail.com>
2022-03-24 01:53:31 +0530

    Revert "added fibonacci logic (#1)" (#2)
    This reverts commit 020fdc83830519f03e02dfc3ea035478526ae073.

--- MathematicalService/settings.py
+++ MathematicalService/settings.py
@@ -134,7 +134,7 @@ STATICFILES_DIRS = []
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': f"redis://:{os.environ.get('REDIS_PASSWORD')}@{os.environ.get('REDIS_URL')}",
+        'LOCATION': os.environ.get('REDIS_URL','redis://127.0.0.1:6379/'),
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
         }
